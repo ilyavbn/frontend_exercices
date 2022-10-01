447664b180a9f34c447cf6dd9f7a23053e23430d
Groz Ilya <ilya.groznii@gmail.com>
2022-10-01 20:27:13 +0300

    sdfsd

---
+++ exercices/prj_5/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(".intro__slider").owlCarousel({
+        items: 4,
+        nav: false,
+        dots: false,
+        margin: 0,
+        stagePadding: 0,
+    });
+});
